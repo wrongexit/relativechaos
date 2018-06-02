639c73b63f6141b0884ae83b114fa20b598e03b7
wrongexit <39079593+wrongexit@users.noreply.github.com>
2018-06-01 20:45:46 -0400

    Update scratch02.js

--- scratch02.js
+++ scratch02.js
@@ -7,8 +7,10 @@ var strox=0;
 function draw() {
   strokeWeight(random(.02*width));
   var p = width*random(1);
-  line(p,0,(p-15)+random(30),height);
-  strox=strox+1;
+    if (mousePressed) {
+    line(p,0,(p-15)+random(30),height);
+    strox=strox+1;
+    }
     if (strox>200) {
     strox=0;
     background(255);
