b55c07cc82029fef27e50d3a9a0a44cbdbb393a7
wrongexit <39079593+wrongexit@users.noreply.github.com>
2018-06-01 20:43:40 -0400

    Update scratch01.js

--- scratch01.js
+++ scratch01.js
@@ -9,8 +9,11 @@ function draw() {
  // fill(255);
   strokeWeight(random(8));
   var p = width*random(1);
-  line(p,0,(p-10)+random(20),height); // this is where it happens
-  strox=strox+1;
+    if (mousePressed) {
+    line(p,0,(p-10)+random(20),height); 
+    strox=strox+1;
+    }
+ 
     if (strox>200) {
     strox=0;
     background(255);
