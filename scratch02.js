fe81e1a9cc2515f7c30f2dbe9244fa97594b8fa0
wrongexit <39079593+wrongexit@users.noreply.github.com>
2018-06-07 02:40:06 -0400

    Update scratch02.js

--- scratch02.js
+++ scratch02.js
@@ -13,18 +13,20 @@ return false;
 } 
 */
        
+stroke(0, 128)
+
 function draw() {
   strokeWeight(random(.02*width));
   var p = width*random(1);
-    //if (mouseIsPressed) {
+    if (mouseIsPressed) {
     line(p,0,(p-15)+random(30),height);
     strox=strox+1;
-    //}
-    
-    //if (touchX>0) {
-    //line(p,0,(p-15)+random(30),height);
-    //strox=strox+1;
-    //}
+    }
+
+    if (touchIsDown) {
+    line(p,0,(p-15)+random(30),height);
+    strox=strox+1;
+    }
   
     if (strox>200) {
     strox=0;
