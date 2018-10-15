b5301b76a5210b3cb18ca37adbf0a359f271b452
wrongexit <39079593+wrongexit@users.noreply.github.com>
2018-10-15 01:36:48 -0400

    Update colorsmear.js

--- colorsmear.js
+++ colorsmear.js
@@ -7,6 +7,6 @@ function setup() {
 var n = 0;
 function draw() {
   n = n + .01;
-  fill( int(noise(n)*256), int(noise(n+100)*256), int(noise(n+200)*256), 10 * noise(n+300) );
+  fill( int(noise(n)*256), int(noise(n+100)*256), int(noise(n+200)*256), 10 * noise(n+300) * 0 + 100);
   rect(noise(n+400) * width, noise(n+500) * height, noise(n+600) * width * 2, noise(n+700) * height * 2);
 }
