e3512d6ae525d710ba55e34a3b106fc927bf8cc5
wrongexit <39079593+wrongexit@users.noreply.github.com>
2018-10-15 01:03:03 -0400

    Add files via upload

---
+++ colorsmearjs.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas (displayWidth, displayHeight-1);
+  background(0);
+  noStroke();
+  rectMode(RADIUS);
+}
+var n = 0;
+function draw() {
+  n = n + .01;
+  fill( int(noise(n)*256), int(noise(n+100)*256), int(noise(n+200)*256), 10 * noise(n+300) );
+  rect(noise(n+400) * width, noise(n+500) * height, noise(n+600) * width, noise(n+700) * width);
+}
